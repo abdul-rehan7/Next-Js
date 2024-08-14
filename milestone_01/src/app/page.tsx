fd7c9f35645851f7ac4c2dae07eae2c62380888c
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-14 11:53:04 +0500

    update

--- milestone_01/src/app/page.tsx
+++ milestone_01/src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className=" lg:text-9xl text-4xl font-bold flex justify-center items-center w-screen h-screen">
-      <p className="bg-gradient-to-b from-purple-500 to-blue-500 bg-clip-text text-transparent">Hellow World</p>
+      <p className="bg-gradient-to-b from-purple-500 to-blue-500 bg-clip-text text-transparent">Hello World!</p>
 
     </div>
   );
