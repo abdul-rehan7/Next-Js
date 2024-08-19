7c75cc300820636785595a7108302ba6eaf57ca5
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-19 11:30:44 +0500

    commit

--- milestone_03/blog_website/app/posts/[title]/page.tsx
+++ milestone_03/blog_website/app/posts/[title]/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function page() {
+export default async function Page({ params }) {
+  const { title } = params;
+  const fetchData = await fetch(`http://localhost:3000/api/posts/${title}`);
+  const res = await fetchData.json();
+
+  console.log("API Response:", res);
+
   return (
-    <div>
-      I am the page
+    <div className="bg-green-500 h-screen p-4">
+      <h1>{res}</h1>
     </div>
-  )
+  );
 }
--- milestone_03/blog_website/components/Section2.tsx
+++ milestone_03/blog_website/components/Section2.tsx
@@ -59,3 +59,4 @@ function Post({ img, title, body }) {
     </div>
   );
 }
+
