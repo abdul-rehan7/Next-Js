dee360af6b321b27cc98183ce6453ea99c58d3f3
rehanmirza76 <abdulrehan0317@gmail.com>
2024-08-19 17:33:26 +0500

    update

--- milestone_03/blog_website/app/posts/[title]/page.tsx
+++ milestone_03/blog_website/app/posts/[title]/page.tsx
@@ -4,14 +4,17 @@ export default async function Page({ params }) {
   const { title } = params;
   const fetchData = await fetch(`http://localhost:3000/api/posts/${title}`);
   const res = await fetchData.json();
-
-  console.log("API Response:", res);
-
   return (
     <div className="text-center flex flex-col items-center py-10 space-y-7 lg:px-[15%] px-[7%] p-4">
       <h1 className="lg:text-4xl text-2xl font-bold">{res.user[0].title}</h1>
-      <img src={res.user[0].img} className="lg:w-[70vw]  lg:h-[80vh] w-full h-[40vh]" alt="" />
-      <p className="lg:text-base text-sm text-justify text-gray-500">{res.user[0].body}</p>
+      <img
+        src={res.user[0].img}
+        className="lg:w-[70vw]  lg:h-[80vh] w-full h-[40vh]"
+        alt=""
+      />
+      <p className="lg:text-base text-sm text-justify text-gray-500">
+        {res.user[0].body}
+      </p>
     </div>
   );
 }
