f900961068a36d43390798da0f70e1376602c1c9
rehanmirza76 <abdulrehan0317@gmail.com>
2024-09-03 19:49:37 +0500

    update

--- ecommerce_web/app/[categories]/[slug]/page.tsx
+++ ecommerce_web/app/[categories]/[slug]/page.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 
 export default async function Page({ params }) {
-  const { name } = params;
-  const fetchData = await fetch(`http://localhost:3000/api/item/${name}`);
+  const { slug } = params.slug; // Destructure to get 'categories'
+  console.log(params); 
+  
+  const fetchData = await fetch(
+    `http://localhost:3000/api/item/${slug}` 
+  );
   const res = await fetchData.json();
 
   console.log("API Response:", res);
 
   return (
     <div className="text-center flex flex-col items-center py-10 space-y-7 lg:px-[15%] px-[7%] p-4">
-      <h1 className="lg:text-4xl text-2xl font-bold">{res.item[0].name}</h1>
-      <img src={res.item[0].img} className="lg:w-[70vw]  lg:h-[80vh] w-full h-[40vh]" alt="" />
-      <p className="lg:text-base text-sm text-justify text-gray-500">{res.item[0].desc}</p>
+      <h1 className="lg:text-4xl text-2xl capitalize font-bold">
+        {res.item[0].name} 
+      </h1>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+        {res.item[0].price}
+      </div>
+
+      <p className="lg:text-base text-sm text-justify text-gray-500"></p>
     </div>
   );
 }
--- ecommerce_web/app/api/item/[name]/route.ts
+++ ecommerce_web/app/api/item/[name]/route.ts
@@ -4,7 +4,7 @@ import data from "@/app/data.json";
 export async function GET(request: Request, context: any) {
   const { params } = context;
   const item = data.filter((x) => params.name == x.name);
-
+  console.log("Filtered items:", item);
   return NextResponse.json({ item });
 }
 
